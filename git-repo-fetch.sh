50356f7bffce24e6b39bdd8d94eb7832777fc0eb
Mingzhu Liu <redisliu@gmail.com>
2016-07-19 21:21:14 +0000

    add git-repo-fetch.sh

---
+++ git-repo-fetch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+. ~/lib-shell/lib.sh
+
+git_repo_clone git://git.apache.org/zookeeper.git release-3.4.8
+git_repo_clone https://github.com/facebook/folly.git
+git_repo_clone https://github.com/google/googletest.git
+git_repo_clone https://github.com/facebook/proxygen.git
+git_repo_clone https://github.com/facebook/fbthrift.git
+git_repo_clone https://github.com/facebook/wangle.git
+git_repo_clone https://github.com/facebook/folly.git
+git_repo_clone https://github.com/facebook/proxygen.git
+git_repo_clone https://github.com/facebook/fbthrift.git
+git_repo_clone https://github.com/facebook/rocksdb.git
--- lib-shell/git.sh
+++ lib-shell/git.sh
@@ -14,3 +14,17 @@ git_commit() {
   git commit --amend "$@"
 }
 
+git_repo_clone() {
+    local url=${1?}
+    local release=${2:-unset}
+
+    local P="${url/*\//}"
+    local dir=${P/%.git/}
+    [ -d $dir ] && return
+    git clone $url
+    pushd $dir
+    if [ "$release" != "unset" ]; then
+        git checkout -b $release $release 
+    fi
+    popd
+}
