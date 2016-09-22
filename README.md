f71a14044625053f44592df138ad6393957366aa
Mingzhu Liu <redisliu@gmail.com>
2016-09-22 17:46:22 +0000

    add read me

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dev-env
 Useful bash script libs
+
+# Install
+
+##  bash setup_env.sh
--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+bindir=`dirname $0`
+bindir=`cd $bindir;pwd`
 
 git config --global user.name "Mingzhu Liu"
 git config --global user.email "redisliu@gmail.com"
 
 git config --global core.autocrlf input
 git config --global core.editor vim
+
+sh $bindir/software-install/vim/init.sh
