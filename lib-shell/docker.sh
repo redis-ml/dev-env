0f9b2dc89e87ec7d952aa4d2ada94826deff068f
mingzhu_liu <mingzhu.liu@airbnb.com>
2018-11-13 14:44:28 -0800

    Improve docker scripts

--- lib-shell/docker.sh
+++ lib-shell/docker.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 clear_docker_containers() {
-  for F in `docker ps -a -q -f status=exited`; do
-    docker rm -v $F
-  done
+  docker ps -aqf status=exited | xargs -I {} docker rm {}
 }
 
 clear_docker_images() {
-  for F in `docker images -f "dangling=true" -q`; do
-    docker rmi $F
-  done
+  docker images -qf "dangling=true" -q | xargs -I {} docker rmi {}
 }
+
+docker_purge_container() {
+  NAME=${1?docker name}
+  docker ps \
+    -a \
+    --format '{{.ID}}\t{{.Names}}' \
+    | awk \
+    -v name=$NAME \
+    '$2==name{system("docker stop "$1" && docker rm "$1)}'
+}
+
