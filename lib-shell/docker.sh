137aa6622d18b7ea4314f0125e06759883c55c69
Mingzhu Liu <mingzhu.liu@robinhood.com>
2020-04-01 17:28:53 -0700

    Fix bugs for zsh

--- lib-shell/docker.sh
+++ lib-shell/docker.sh
@@ -32,4 +32,4 @@ docker_mysql() {
 
 }
 
-docker_mysql
+# docker_mysql
--- lib-shell/lib.sh
+++ lib-shell/lib.sh
@@ -6,7 +6,7 @@ export PATH=${MY_OLD_PATH}
 
 if [ "${MY_ENV_SCRIPT_DIR}" != "" ]; then
   for F in `find ${MY_ENV_SCRIPT_DIR} -maxdepth 1 -type f -name "lib.sh" -prune -o  -type f -name "*.sh" -print`; do
-      if [ "MY_DEV_ENV_DEBUG" == "true" ]; then
+      if [ "MY_DEV_ENV_DEBUG" = "true" ]; then
           echo $F
       fi
       . $F
