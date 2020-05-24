ccf4b91ac46ff5b962ad0cc072a522fe92cfccf3
Mingzhu Liu <redisliu@gmail.com>
2020-05-23 23:22:35 -0700

    Sync code

--- lib-shell/golang.sh
+++ lib-shell/golang.sh
@@ -2,19 +2,6 @@
 
 export GO111MODULE=on
 
-GOROOT="$(go env GOROOT 2>/dev/null)"
-
-if [ "$GOROOT" = "" ]; then
-  export GOROOT=/usr/local/opt/go/libexec
-fi
-
-GOPATH="$(go env GOPATH 2>/dev/null)"
-if [ "$GOPATH" = "" ]; then
-  GOPATH="$(cd ~/go;pwd)"
-fi
-
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-
 go_install() {
   local SUB=${1?Need package relative path}
   go install -ldflags="-X main.Version=$(git describe --always --long --dirty)" ${MY_PACKAGE_NAME}/$SUB
