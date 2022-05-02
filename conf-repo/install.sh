84839762ecd02cb44d61f8ee35e493e7c1b0377e
Mingzhu Liu <redisliu@gmail.com>
2022-05-02 05:24:40 +0000

    [cleanup] Move deprecated files to scripts/

--- conf-repo/install.sh
+++
@@ -1,7 +0,0 @@
-
-F_L=$(find ./ -maxdepth 1 -mindepth 1 -type d)
-
-for D in $F_L; do
-    echo $D;
-    sh -c "cd $D; sh install.sh"
-done
--- conf-repo/m2/install.sh
+++
@@ -1,4 +0,0 @@
-# Install maven settings conf
-
-echo "[Warning] Maven config files not installed..."
-#mkdir -p ~/.m2 && cp settings.xml ~/.m2/
